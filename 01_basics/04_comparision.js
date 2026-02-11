4f7f683d153492c6ae7f31bc6d52c0d5ce28fd63
Raees <raees.fatima.7070@gmail.com>
2026-02-11 16:39:44 +0530

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,24 @@
+// This is not a clean code.
+
+//dont write does like this. It is not a good practice.
+console.log(2 > 1);
+console.log(2 >= 1);
+console.log(2 < 1);
+console.log(2 == 1);
+console.log(2 != 1);
+
+
+console.log("2" > 1);
+console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === 
+
+console.log("2" === 2);
