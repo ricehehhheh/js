289cf7f8dfbe25bffc7dd7ffe7428c9eea11374c
Raees <raees.fatima.7070@gmail.com>
2026-02-18 11:19:41 +0530

    'from', 'is', 'of' array operators

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -14,3 +14,13 @@ const another_array = [1,3,5,6, [2,4,6,5], 55, [5,6,7,3,[56,7]]];
 const real_array = another_array.flat(Infinity);
 console.log(real_array);
 
+
+console.log(Array.isArray("Raees"));
+console.log(Array.from("Raees"));
+console.log(Array.from({name: "Raees", age: 23})); //interesting 
+
+let score1 = 100
+let score2 = 200
+let score3 = 300        
+
+console.log(Array.of(score1, score2, score3));
